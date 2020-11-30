7dfba39fb26073c2d6d6343ed9826f52f7f9def1
Muhammad <azhar40@live.co.uk>
2020-11-30 22:56:32 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,112 @@
+
+// Question 1 
+
+// var arr = []
+// var total=0;
+// for (let i = 0 ;i<10;i++)
+// {
+// arr.push(+prompt(`enter value of number ${i}`));
+// total+= arr[i];
+// console.log(`Total is ${total}`);
+// console.log(arr);
+// }
+// let average = total / arr.length ; 
+// console.log(average);
+
+//  Question 2 
+
+// var table = 15;
+
+// for (var i = 0 ;i<=10;i++)
+// {
+//     console.log(`15*${i}=${15*i}`);
+// }
+
+
+//  Question 3 
+
+
+// var combinedArr = [1,2,3,4,5,6,7,8,9,10];
+// var even = [];
+// var odd = [];
+
+
+// for (let i = 0 ; i<combinedArr.length;i++)
+// {
+//     if ( combinedArr[i] % 2 === 0 )
+//     {
+//         even.push(combinedArr[i]);
+//     }
+//     else{
+//         odd.push(combinedArr[i]);
+//     }
+//     console.log("even numbers are ===>",even);
+//     console.log("Odd numbers are ===>",odd);
+// }
+
+//  Question 4 
+// for(i=1;i<=6;i++)
+// {
+//  for(j=1;j<=i;j++)
+//    {
+//  document.write("*");
+// }
+// document.write("<br>");
+// }
+
+
+// Question 5
+
+// for(i=1;i<=6;i++)
+// {
+//  for(j=1;j<=i;j++)
+//    {
+//  document.write("",j);
+// }
+// document.write("<br>");
+// }
+
+// Question 6
+
+// for(i=1;i<=6;i++)
+// {
+//  for(j=1;j<=i;j++)
+//    {
+//  document.write("",i);
+// }
+// document.write("<br>");
+// }
+
+//  Question 7
+
+// var fact = 5;
+
+// for (let i = 5-1 ;i>=0;i--)
+// {
+//    fact = fact*i;
+//    document.write("factorial is ",fact);
+    
+// }
+
+//  Question 8 
+
+
+
+
+//  Question 9
+ 
+// var name = "123456";
+// var updated = "";
+// for (let i = name.length-1 ; i>=0;i--)
+// {
+//     updated+=name[i];
+//     if ( i===0)
+//     {
+//         console.log("updated numbers are==>",updated);
+//     }
+// }
+
+
+
+
+
